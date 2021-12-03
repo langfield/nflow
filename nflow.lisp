c568845738e7bac7ffb6426bb7951513fcaa388f
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2021-12-03 21:57:36 +0000

    Added a function that reads in a file.
    Learned how to print lines.

--- nflow.lisp
+++ nflow.lisp
@@ -1,9 +1,18 @@
+; Read in a file.
 (defun get-file (filename)
   (with-open-file (stream filename)
     (loop for line = (read-line stream nil)
           while line
           collect line)))
 
+; Print an arbitrary list.
+(defun print-list (list)
+ (format t "~{~A ~}~%" list))
+
+; Main function.
 (defun main (argv)
-    (write-line "hello world")
-    (write-line argv))
+  (write-line "hello world")
+  (print-list argv)
+  (setf readme-lines (get-file (nth 1 argv)))
+  (print-list readme-lines)
+  )
