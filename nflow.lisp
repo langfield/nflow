deb1dbf8e688fd635896cd93921a1e683083eaa8
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2021-12-07 20:15:42 +0000

    It works.

--- nflow.lisp
+++ nflow.lisp
@@ -7,10 +7,23 @@
 
 ; Print an arbitrary list.
 (defun print-list (printable-list)
- (format t "~{~A ~}~%" printable-list))
+  (format t "~{~A ~}~%" printable-list))
+
+; Check whether STR starts with PREFIX.
+(defun starts-with (prefix)
+  (defun starts-with-prefix (str)
+    (cond
+      ; If STR and PREFIX match exactly, then MISMATCH returns NIL, so we must
+      ; return T.
+      ((equal (mismatch str prefix) NIL) t)
+      ; Return T if PREFIX is a substring of STR.
+      ((>= (mismatch str prefix) 2) t)
+      ; Return NIL otherwise.
+      (t) (NIL))))
 
 ; Main function.
 (defun main (argv)
-  (write-line "hello world")
-  (print-list argv)
-  (print-list (get-file (nth 1 argv))))
+  (print-list (get-file (nth 1 argv)))
+  (terpri)
+  (terpri)
+  (print-list (remove-if (starts-with "- ") (get-file (nth 1 argv)))))
