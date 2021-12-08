524655ef1eb274f6257310017733284ee5d10912
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2021-12-08 00:00:23 +0000

    Started debugging.

--- nflow.lisp
+++ nflow.lisp
@@ -12,18 +12,22 @@
 ; Check whether STR starts with PREFIX.
 (defun starts-with (prefix)
   (defun starts-with-prefix (str)
+    (print (list "Mismatch expression:" (mismatch str prefix)))
+    (print (list "Equal expression:" (equal (mismatch str prefix) NIL)))
+    (print (list ">= expression:" (>= (mismatch str prefix) 2)))
     (cond
       ; If STR and PREFIX match exactly, then MISMATCH returns NIL, so we must
       ; return T.
-      ((equal (mismatch str prefix) NIL) t)
+      ((equal (mismatch str prefix) NIL) (print "strict equality branch"))
       ; Return T if PREFIX is a substring of STR.
-      ((>= (mismatch str prefix) 2) t)
+      ((>= (mismatch str prefix) 2) (print "substring branch"))
       ; Return NIL otherwise.
-      (t) (NIL))))
+      (t (print "else branch")))))
 
 ; Main function.
 (defun main (argv)
-  (print-list (get-file (nth 1 argv)))
-  (terpri)
-  (terpri)
-  (print-list (remove-if (starts-with "- ") (get-file (nth 1 argv)))))
+  (print (list "'- hello' starts with '- ':" (funcall (starts-with "- ") "- hello")))
+  (print (list "'hello' starts with '- ':" (funcall (starts-with "- ") "hello")))
+  (write-line ""))
+  ; (print-list (get-file (nth 1 argv)))
+  ; (print-list (remove-if (starts-with "- ") (get-file (nth 1 argv)))))
