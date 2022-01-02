014925133f7e18aa5e80b536aedb27f56b84905b
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2022-01-02 00:58:09 -0500

    Added parinfer-rust styling.

--- nflow.lisp
+++ nflow.lisp
@@ -36,7 +36,7 @@
   " Get ``lst[:num]``. "
   (if (> num 0)
     (cons (car lst) (get-n-items (cdr lst) (- num 1)))
-      '()))
+    '()))
 
 (defun slice (lst start size)
   " Take a slice of ``lst`` of the form ``lst[start:start + size]``. "
@@ -58,18 +58,18 @@
     (format t "~A~%" (car lst))
     (setq lst (cdr lst))))
 
-(defun in-list (x lst) 
+(defun in-list (x lst)
   " Check whether X is in LST. "
    (cond
-      ((null lst) ()) 
-      ((equal (car lst) x) lst) 
+      ((null lst) ())
+      ((equal (car lst) x) lst)
       (t (in-list x (cdr lst)))))
 
-(defun subset (lst1 lst2) 
+(defun subset (lst1 lst2)
   " Check whether every element in LST1 is in LST2. "
-   (cond 
-      ((null lst1) t) 
-      ((in-list (car lst1) lst2)(subset (cdr lst1) lst2)) 
+   (cond
+      ((null lst1) t)
+      ((in-list (car lst1) lst2)(subset (cdr lst1) lst2))
       (t ())))
 
 (defun list-equals (lst1 lst2)
@@ -83,86 +83,86 @@
 
 (defun make-tree (item)
    "it creates a new node with item."
-   (cons (cons item nil) nil)
-)
+   (cons (cons item nil) nil))
+
 
 (defun first-child (tree)
     " Access first child. "
    (if (null tree)
       nil
-      (cdr (car tree))
-   )
-)
+      (cdr (car tree))))
+
+
 
 (defun next-sibling (tree)
   " Get next sibling. "
-   (cdr tree)
-)
+   (cdr tree))
+
 
 (defun data (tree)
   " Get data of tree. "
-   (car (car tree))
-)
+   (car (car tree)))
+
 
 (defun add-child (tree child)
   " Add child to tree. "
    (setf (car tree) (append (car tree) child))
-   tree
-)
+   tree)
+
 
 (defun count-leading-spaces (s)
   " Count leading spaces of a string. "
   (let*
     (
-      (left-trimmed-s (str:trim-left s))
-    )
-    (- (length s) (length left-trimmed-s))
-  )
-)
+      (left-trimmed-s (str:trim-left s)))
+
+    (- (length s) (length left-trimmed-s))))
+
+
 
 (defun unroll-parent-stack (parent-stack num-indents)
   " Go up NUM-INDENTS levels, returning the parent and the parent-stack. "
   (assert (> num-indents 0))
   (let*
     (
-      (parent nil)
-    )
+      (parent nil))
+
     (dotimes (i num-indents)
 
       ; Get PARENT from PARENT-STACK.
       (setq parent (last parent-stack))
 
       ; Remove PARENT from PARENT-STACK.
-      (setq parent-stack (butlast parent-stack))
-    )
-    (list parent-stack parent)
-  )
-)
+      (setq parent-stack (butlast parent-stack)))
+
+    (list parent-stack parent)))
+
+
 
 
 (defun get-indent-size (delta indent-size)
   " Get the indent size. "
   (if (> indent-size 0)
     indent-size
-    (abs delta)
-  )
-)
+    (abs delta)))
+
+
 
 (defun get-num-indents (indent-level old-indent-level indent-size line)
   " Compute NUM-INDENTS and INDENT-SIZE from the indent levels. "
   (let*
     (
-      (indent-delta 0)
-    )
+      (indent-delta 0))
+
     (setq indent-delta (- indent-level old-indent-level))
     (setq indent-size (get-indent-size indent-delta indent-size))
     (if (not (equal (mod indent-delta indent-size) 0))
-      (error "Inconsistent indentation on line with contents: ~A~%" line)
-    )
+      (error "Inconsistent indentation on line with contents: ~A~%" line))
+
     ; Return (NUM-INDENTS, INDENT-SIZE).
-    (list (floor indent-delta indent-size) indent-size)
-  )
-)
+    (list (floor indent-delta indent-size) indent-size)))
+
+
 
 
 (defun parse-todo-tree (lst)
@@ -180,8 +180,8 @@
       (parent nil)
       (parent-stack-pair nil)
       (indent-num-size-pair nil)
-      (num-indents 0)
-    )
+      (num-indents 0))
+
     ; Loop over LST
     (for:for ((line over lst))
 
@@ -202,15 +202,13 @@
               (setq indent-size (second indent-num-size-pair))
               (assert (> num-indents 0))
               (if (> num-indents 1)
-                (error "Indent level increased by more than 1: ~A~%" line)
-              )
+                (error "Indent level increased by more than 1: ~A~%" line))
 
               ; Append NODE to PARENT-STACK.
               (setq parent-stack (append parent-stack node))
 
               ; Set NODE to LAST-CREATED-CHILD.
-              (setq node last-created-child)
-            )
+              (setq node last-created-child))
 
             ; Otherwise if INDENT-LEVEL decreased:
             (if (< indent-level old-indent-level)
@@ -228,24 +226,17 @@
                 (setq parent (second parent-stack-pair))
 
                 ; Set NODE to PARENT.
-                (setq node parent)
-              )
-            )
-          )
+                (setq node parent))))
 
           ; Add LINE as another child.
           (setq last-created-child (make-tree (str:trim-left line)))
-          (setq node (add-child node last-created-child))
-        )
-      )
+          (setq node (add-child node last-created-child))))
+
       ; Get a reference to the root.
       (if (> (length parent-stack) 0)
         (setq tree (car parent-stack))
-        (setq tree node)
-      )
+        (setq tree node)))
 
-
-    )
     ; Draw TREE on each iteration.
     (terpri)
     (draw-cons-tree:draw-tree tree)
@@ -253,32 +244,23 @@
 
     ; Print cons form of TREE.
     (format t "Tree: ~A~%" tree)
-    tree
-  )
-)
+    tree))
+
 
 
 (defun check-no-undashed-lines-above-delimiter (lines position-of-empty-line)
-  (let*
-    (
-      (i 0)
-    )
-    (for:for ((line over lines))
-      (if (not (str:starts-with? "- " line))
-        (progn
-          (if (< i position-of-empty-line)
-            (error "Line: '~A' is above delimiter on line: ~A~%" line position-of-empty-line)
-          )
-          (if (equal i position-of-empty-line)
-            (assert (equal line ""))
-          )
-        )
-      )
-      (setq i (1+ i))
-    )
-    t
-  )
-)
+    """Check that there are no undashed lines above delimiter."""
+    (let*
+      ((i 0))
+      (for:for ((line over lines))
+        (if (not (str:starts-with? "- " line))
+          (progn
+            (if (< i position-of-empty-line)
+              (error "Line: '~A' is above delimiter on line: ~A~%" line position-of-empty-line))
+            (if (equal i position-of-empty-line)
+              (assert (equal line "")))))
+        (setq i (1+ i)))
+      t))
 
 
 (defun reflow-nontrivial-lines (lines)
@@ -290,8 +272,8 @@
       (lines-above-delimiter nil)
       (lines-below-delimiter nil)
       (num-lines-below-delimiter nil)
-      (dashed-lines-below-delimiter nil)
-    )
+      (dashed-lines-below-delimiter nil))
+
     ; Find the empty line (delimiter).
     (setq position-of-empty-line (position "" lines :test #'string=))
     (check-no-undashed-lines-above-delimiter lines position-of-empty-line)
@@ -315,9 +297,9 @@
     (parse-todo-tree lines)
 
     ; Concatenate everything, adding delimiter back in.
-    (concatenate 'list lines-above-delimiter dashed-lines-below-delimiter '("") undashed-lines)
-  )
-)
+    (concatenate 'list lines-above-delimiter dashed-lines-below-delimiter '("") undashed-lines)))
+
+
 
 
 (defun reflow-dashed-lines (lines)
@@ -326,17 +308,15 @@
     '()
     (if (equal (position "" lines :test #'string=) nil)
       lines
-      (reflow-nontrivial-lines lines)
-    )
-  )
-)
+      (reflow-nontrivial-lines lines))))
+
 
 (defun main (argv)
   " Main function. "
   (let*
     ((lines (get-file (nth 1 argv)))
-    (reflowed-lines (reflow-dashed-lines lines)))
-  (dump-lines reflowed-lines)))
+     (reflowed-lines (reflow-dashed-lines lines)))
+   (dump-lines reflowed-lines)))
 
 
 ; OUTLINE
